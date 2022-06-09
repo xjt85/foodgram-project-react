ebba1e74cfac0ab95cff35990ed2375e8276f9c6
Роман Чуклинов <xjt@mail.ru>
2022-06-09 22:41:49 +1000

    Проработка моделей приложения recipes

--- backend/foodgram_backend/recipes/models.py
+++ backend/foodgram_backend/recipes/models.py
@@ -3,26 +3,51 @@ from django.db import models
 
 User = get_user_model()
 
+
 class Recipe(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
-    caption = models.TextField()
-    image = models.ImageField(
-        upload_to='posts/', null=True, blank=True)
-    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
-    group = models.ForeignKey(
-        Group, on_delete=models.SET_NULL,
-        related_name="posts", blank=True, null=True
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='recipes',
+        verbose_name='Автор',
     )
+    name = models.CharField(
+        max_length=256,
+        verbose_name='Название',
+    )
+    image = models.ImageField(
+        upload_to='recipes/', null=False, blank=True)
+    text = models.TextField(null=False, verbose_name='Описание')
+    ingredients = models.ManyToManyField(Ingredient, through='RecipeIngredient')
+    tag = models.ForeignKey(User, on_delete=models.CASCADE, related_name='recipes')
+    cooking_time = models.IntegerField()
 
     def __str__(self):
         return self.text
 
-# Рецепт должен описываться такими полями:
-# Автор публикации (пользователь).
-# Название.
-# Картинка.
-# Текстовое описание.
-# Ингредиенты: продукты для приготовления блюда по рецепту. Множественное поле, выбор из предустановленного списка, с указанием количества и единицы измерения.
-# Тег (можно установить несколько тегов на один рецепт, выбор из предустановленных).
-# Время приготовления в минутах.
-# Все поля обязательны для заполнения.
+
+class Tag(models.Model):
+    name = models.CharField(
+        max_length=256,
+        verbose_name='Название',
+    )
+    color_code = models.CharField(
+        max_length=256,
+        verbose_name='Цветовой код',
+    )
+    slug = models.SlugField(
+        max_length=50,
+        unique=True,
+        verbose_name='Slug',
+    )
+
+
+class Ingredient(models.Model):
+    name = models.CharField("Название", max_length=200)
+    measurement_unit = models.CharField("Единица измерения", max_length=25)
+
+
+class RecipeIngredient(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    amount = models.SmallIntegerField("Количество")
--- backend/foodgram_backend/users/models.py
+++ backend/foodgram_backend/users/models.py
@@ -1,3 +1,26 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    USER = 'user'
+    MODERATOR = 'moderator'
+    ADMIN = 'admin'
+    ROLES = ((USER, 'USER'), (MODERATOR, 'MODERATOR'), (ADMIN, 'ADMIN'))
+
+    email = models.EmailField(max_length=254, unique=True, blank=False)
+    first_name = models.CharField(max_length=150, blank=True)
+    last_name = models.CharField(max_length=150, blank=True)
+    bio = models.TextField(verbose_name='Биография', blank=True)
+    role = models.CharField(max_length=300, choices=ROLES, default=ROLES[0][0])
+
+    @property
+    def is_admin(self):
+        return self.is_superuser or self.role == self.ADMIN
+
+    @property
+    def is_moderator(self):
+        return self.role == self.MODERATOR
+
+    def __str__(self):
+        return self.username
