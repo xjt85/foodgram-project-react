f6fc3cf5adbc2a5a260360c610273c8c48ec2bb5
Роман Чуклинов <xjt@mail.ru>
2022-06-21 00:14:36 +1000

    проработка моделей

--- backend/foodgram_backend/recipes/models.py
+++ backend/foodgram_backend/recipes/models.py
@@ -4,6 +4,51 @@ from django.db import models
 User = get_user_model()
 
 
+class Ingredient(models.Model):
+    name = models.CharField(max_length=200, verbose_name="Название")
+    measurement_unit = models.CharField(max_length=200, verbose_name='Единица измерения')
+
+    class Meta:
+        ordering = ['-id']
+        verbose_name = 'Ингредиент'
+        verbose_name_plural = 'Ингредиенты'
+        constraints = [
+            models.UniqueConstraint(fields=['name', 'measurement_unit'],
+                                    name='unique ingredient')
+        ]
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    RED = '#FF0080'
+    GREEN = '#00FF00'
+    BLUE = '#0000FF'
+
+    COLOR_CHOICES = [
+        (RED, 'Красный'),
+        (GREEN, 'Зеленый'),
+        (BLUE, 'Синий'),
+    ]
+    name = models.CharField(max_length=200, unique=True,
+                            verbose_name='Название тега')
+    color_code = models.CharField(max_length=7, unique=True, choices=COLOR_CHOICES,
+                             verbose_name='Цвет в HEX-формате')
+    slug = models.SlugField(max_length=200, unique=True,
+                            verbose_name='Уникальный слаг')
+
+    class Meta:
+        ordering = ['-id']
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
+
+    def __str__(self):
+        return self.name
+
+# --------------------------- ddddddddddddddddddddddd ------------------------------------
+
+
 class Recipe(models.Model):
     author = models.ForeignKey(
         User,
@@ -42,9 +87,7 @@ class Tag(models.Model):
     )
 
 
-class Ingredient(models.Model):
-    name = models.CharField("Название", max_length=200)
-    measurement_unit = models.CharField("Единица измерения", max_length=25)
+
 
 
 class RecipeIngredient(models.Model):
