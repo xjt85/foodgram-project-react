024aba5648be050bcbf3eda695e5053e32b7890a
Роман Чуклинов <xjt@mail.ru>
2022-06-22 13:27:06 +1000

    проработка моделей, роутинга, сериализаторов и т.д.

--- backend/foodgram_backend/users/models.py
+++ backend/foodgram_backend/users/models.py
@@ -1,26 +1,30 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth import get_user_model
 from django.db import models
 
+User = get_user_model()
 
-class User(AbstractUser):
-    USER = 'user'
-    MODERATOR = 'moderator'
-    ADMIN = 'admin'
-    ROLES = ((USER, 'USER'), (MODERATOR, 'MODERATOR'), (ADMIN, 'ADMIN'))
 
-    email = models.EmailField(max_length=254, unique=True, blank=False)
-    first_name = models.CharField(max_length=150, blank=True)
-    last_name = models.CharField(max_length=150, blank=True)
-    bio = models.TextField(verbose_name='Биография', blank=True)
-    role = models.CharField(max_length=300, choices=ROLES, default=ROLES[0][0])
+class Follow(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='follower',
+        verbose_name='Подписчик',
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='following',
+        verbose_name='Автор',
+    )
 
-    @property
-    def is_admin(self):
-        return self.is_superuser or self.role == self.ADMIN
-
-    @property
-    def is_moderator(self):
-        return self.role == self.MODERATOR
-
-    def __str__(self):
-        return self.username
+    class Meta:
+        ordering = ['-id']
+        verbose_name = 'Подписка'
+        verbose_name_plural = 'Подписки'
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'author'],
+                name='unique follow',
+            )
+        ]
