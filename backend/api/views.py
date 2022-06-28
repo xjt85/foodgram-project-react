5b3dc5a90c03e80e17d85b85da475d135e0e638a
Роман Чуклинов <xjt@mail.ru>
2022-06-28 13:24:06 +1000

    Доработка для workflow

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,12 +1,12 @@
 from api.filters import AuthorAndTagFilter, IngredientSearchFilter
-from recipes.models import (Cart, Favorite, Ingredient, IngredientAmount,
-                            Recipe, Tag)
 from api.pagination import LimitPageNumberPagination
 from api.permissions import IsAdminOrReadOnly, IsOwnerOrReadOnly
 from api.serializers import (CropRecipeSerializer, IngredientSerializer,
                              RecipeSerializer, TagSerializer)
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
+from recipes.models import (Cart, Favorite, Ingredient, IngredientAmount,
+                            Recipe, Tag)
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
 from reportlab.pdfgen import canvas
